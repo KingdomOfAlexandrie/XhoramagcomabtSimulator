3a181d2d97df321bc2f72e815f4519d2dffa3cbb
KingdomOfAlexandrie <52180582+KingdomOfAlexandrie@users.noreply.github.com>
2019-06-24 17:58:43 -0400

    Add files via upload

---
+++ Xhormag combat simulator/Xhormag combat simulator/Inventory/Armor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xhormag_combat_simulator.Inventory
+{
+    class Armor
+    {
+        //Armor members
+        private string mArmorName;
+        private string mArmorType;
+        private int mArmorValue;
+
+        public Armor(string pName, string pArmorType, int pAmrorValue)
+        {
+            mArmorName = pName;
+            mArmorType = pArmorType;
+            mArmorValue = pAmrorValue;
+        }        //Armor getter
+        public string GetArmorName() => mArmorName;
+        public string GetArmorType() => mArmorType;
+        public int GetArmorValue() => mArmorValue;
+    }
+}
---
+++ Xhormag combat simulator/Xhormag combat simulator/Inventory/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xhormag_combat_simulator.Inventory
+{
+    class Weapon
+    {
+        //Weapon members
+        private string mWeaponName;
+        private int mAbilityBonus;
+        private int mDamageBonus;
+
+        public Weapon(string pName, int pAbiltityBonus, int pDamageBonus)
+        {
+            mWeaponName = pName;
+            mAbilityBonus = pAbiltityBonus;
+            mDamageBonus = pDamageBonus;
+        }
+
+        //Weapon getter
+        public string GetWeaponName() => mWeaponName;
+        public int GetWeaponAbilityBonus() => mAbilityBonus;
+        public int GetWeaponDamageBonus() => mDamageBonus;
+    }
+}
